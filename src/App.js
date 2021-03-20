db92bce7326e0679fc442a169a447bdc6db6ffb8
karthikeyan2002 <bkarthi1000@gmail.com>
2021-03-21 05:01:34 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { version } from 'react'
+import './App.css';
+import Header from './Header.js';
+import Input from './Input.js';
+import Login from './login.js';
+
+export let islogin=true;
+console.log(islogin);
+
+function App() {
+    return (
+        <>
+                         
+           { islogin ? 
+                <>
+                <Header/>
+                <input type="text" placeholder="Search" style={{float:'right'}} />
+                <Input/>
+                </> 
+            :
+                <Login />
+            }
+               
+        </>
+    )
+}
+
+export default App
+
