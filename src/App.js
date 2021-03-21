ae9e19bdddb2a75b3134ff25659be48ed125fa98
Avinash <53227835+Avinashroffl@users.noreply.github.com>
2021-03-21 14:00:13 +0530

    UI improvements

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { version } from 'react'
-import './App.css';
-import Header from './Header.js';
-import Input from './Input.js';
-import Login from './login.js';
+import React, { version } from "react";
+import "./App.css";
+import Header from "./Header.js";
+import Input from "./Input.js";
+import Login from "./login.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { NavigationBar } from "./NavigationBar";
 
-export let islogin=true;
+export let islogin = true;
 console.log(islogin);
 
 function App() {
-    return (
+  return (
+    <>
+      {islogin ? (
         <>
-                         
-           { islogin ? 
-                <>
-                <Header/>
-                <input type="text" placeholder="Search" style={{float:'right'}} />
-                <Input/>
-                </> 
-            :
-                <Login />
-            }
-               
+          <Header />
+          <NavigationBar />
+          <Input />
         </>
-    )
+      ) : (
+        <Login />
+      )}
+    </>
+  );
 }
 
-export default App
-
+export default App;
--- src/Input.js
+++ src/Input.js
@@ -1,55 +1,75 @@
-import React from 'react'
+import React from "react";
 
 function Input() {
-    return (
-             
-        <div className="Input">
-        <table>
-            <tr>
-                <th>  Name </th>
-                <th>  Age </th>
-                <th>  Phone Number </th>
-                <th>  Gender </th>
-                <th>  Diabetes </th>
-                <th>  Hypertension </th>
-                <th>  Epilepsy </th>
-                <th>  Add </th>
-            </tr>
-            <tr>
-                 <td> <input name="Patient-Name" type="text" placeholder="Enter Patient's Name"/> </td>
-                 <td> <input name="Patient-Age" type="number" placeholder="Enter Patient's Age"/> </td>
-                 <td> <input name="Phone-number" type="number" placeholder="Patient's Phone number"/> </td>
-                 <td>
-                     <select>
-                     <option value="Male">Male</option>
-                     <option value="Female">Female</option>
-                     </select>
-                 </td>
-                 <td>
-                     <select>
-                         <option value="Diabetes-num">Not Under Medication</option>
-                         <option value="Diabetes-um">Under Medication</option>
-                     </select>
-                 </td>
-                 <td>
-                     <select>
-                         <option value="hypertension-num">Not Under Medication</option>
-                         <option value="hypertension-um">Under Medication</option>
-                     </select>
-                 </td>
-                 <td>
-                     <select>
-                         <option value="Epilepsy-num">Not under Medication</option>
-                         <option value="Epilepsy-um">Under Medication</option>
-                     </select>
-                 </td>
-                 <td>
-                     <button type="submit">Add</button>
-                 </td>
-           </tr>
-     </table>
-   </div>
-    )
+  return (
+    <div className="Input">
+      <table>
+        <tr>
+          <th> Name </th>
+          <th> Age </th>
+          <th> Phone Number </th>
+          <th> Gender </th>
+          <th> Diabetes </th>
+          <th> Hypertension </th>
+          <th> Epilepsy </th>
+          <th> Add </th>
+        </tr>
+        <tr>
+          <td>
+            {" "}
+            <input
+              name="Patient-Name"
+              type="text"
+              placeholder="Enter Patient's Name"
+            />{" "}
+          </td>
+          <td>
+            {" "}
+            <input
+              name="Patient-Age"
+              type="number"
+              placeholder="Enter Patient's Age"
+            />{" "}
+          </td>
+          <td>
+            {" "}
+            <input
+              name="Phone-number"
+              type="number"
+              placeholder="Patient's Phone number"
+            />{" "}
+          </td>
+          <td>
+            <select>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </td>
+          <td>
+            <select>
+              <option value="Diabetes-num">Not Under Medication</option>
+              <option value="Diabetes-um">Under Medication</option>
+            </select>
+          </td>
+          <td>
+            <select>
+              <option value="hypertension-num">Not Under Medication</option>
+              <option value="hypertension-um">Under Medication</option>
+            </select>
+          </td>
+          <td>
+            <select>
+              <option value="Epilepsy-num">Not under Medication</option>
+              <option value="Epilepsy-um">Under Medication</option>
+            </select>
+          </td>
+          <td>
+            <button type="submit">Add</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
-export default Input
+export default Input;
