bd326f6bbfd7e74f8d5f61b91088e98321d61d1f
Avinash <53227835+Avinashroffl@users.noreply.github.com>
2021-03-25 14:09:03 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -5,25 +5,26 @@ import Login from "./login.js";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { NavigationBar } from "./NavigationBar";
 import { createNewPost } from "./utils";
+import MySurvey from "../src/surveydisp/surveyone";
 export let islogin = true;
 console.log(islogin);
 createNewPost()
-	.then((res) => console.log(res))
-	.catch((err) => console.log(err));
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
 function App() {
-	return (
-		<>
-			{islogin ? (
-				<>
-					<Header />
-					<NavigationBar />
-					<SurveyComponent />
-				</>
-			) : (
-				<Login />
-			)}
-		</>
-	);
+  return (
+    <>
+      {islogin ? (
+        <>
+          <Header />
+          <NavigationBar />
+          <MySurvey />
+        </>
+      ) : (
+        <Login />
+      )}
+    </>
+  );
 }
 
 export default App;
