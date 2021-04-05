c8c87e1599c6535dde6cf60d3f6e1110fd3f0063
avinashr <rajnash69@gmail.com>
2021-04-05 12:14:24 +0530

    qns

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { NavigationBar } from "./NavigationBar";
 import { createNewPost } from "./utils";
 import MySurvey from "../src/surveydisp/surveyone";
-export let islogin = false;
+export let islogin = true;
 console.log(islogin);
 createNewPost()
-	.then((res) => console.log(res))
-	.catch((err) => console.log(err));
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
 function App() {
-	return (
-		<>
-			{islogin ? (
-				<>
-					<Header />
-					<NavigationBar />
-					<MySurvey />
-				</>
-			) : (
-				<Login />
-			)}
-		</>
-	);
+  return (
+    <>
+      {islogin ? (
+        <>
+          <Header />
+          <NavigationBar />
+          <MySurvey />
+        </>
+      ) : (
+        <Login />
+      )}
+    </>
+  );
 }
 
 export default App;
