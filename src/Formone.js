c79aa86abdbdc317ef1ff131241e5c57226694be
avinashr <rajnash69@gmail.com>
2021-04-21 18:55:35 +0530

    ui

--- src/Formone.js
+++ src/Formone.js
@@ -285,7 +285,7 @@ const Formone = () => {
 
 				<button
 					type='submit'
-					class='bg-gray-700 hover:bg-blue-700 text-white hover:text-white font-bold py-2 px-4 border border-gray-500 rounded'
+					class='bg-blue-500 hover:bg-blue-700 text-white hover:text-white font-bold py-2 px-4 border border-gray-500 rounded'
 				>
 					Submit
 				</button>
