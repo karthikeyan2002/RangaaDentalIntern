86acbe55c84314391a46c90bc878d812d332b007
karthikeyan2002 <bkarthi1000@gmail.com>
2021-04-30 10:50:20 +0530

    Dashboard ui

--- src/TableTest.js
+++ src/TableTest.js
@@ -4,9 +4,10 @@ import { getPatients } from "./utils/index";
 import Header from "./Header";
 import NavigationBar from "./NavigationBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPowerOff, faBars , faSearch , faChevronLeft , faChevronRight , faUserPlus} from "@fortawesome/free-solid-svg-icons";
+import { faPowerOff, faBars , faSearch , faChevronLeft , faChevronRight , faUserPlus , faChevronCircleUp} from "@fortawesome/free-solid-svg-icons";
 import "./Styles/TableTest.css"
 import { divide } from "lodash";
+import { Button } from "react-bootstrap";
 
 let today = new Date().toLocaleString();
 
@@ -47,9 +48,18 @@ function TableTest() {
 				</p>
 
 				<p className="patient"> Number of patients visited : 200 </p>
+
+				<a href="#section2">
+				<FontAwesomeIcon
+					icon={faChevronCircleUp}
+					className='text-white absolute -bottom-3 right-4'
+					size='2x'
+				></FontAwesomeIcon>
+				</a>
+				
 			</div>
-			<NavigationBar />		
-<div className="section2">
+			{/* <NavigationBar />		 */}
+<div className="section2" id="section2">
 
 	<div className="w-1/6 float-right search-icon">
 	<div class="p-8">
@@ -77,14 +87,16 @@ function TableTest() {
                     <table className="min-w-full">
                         <thead className="text-white">
                             <tr>
-                                <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4  tracking-wider uppercase">Full Name</th>
-                                <th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Age</th>
-								<th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Gender</th>
-                                <th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Phone</th>
-                                <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 tracking-wider uppercase">Email</th>
-                                <th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Status</th>
-                                <th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Details</th>
-								</tr>
+                                <th className="pl-4 py-4 border-b-2 border-gray-300 text-left leading-4  tracking-wider uppercase">Full Name</th>
+                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Age</th>
+								<th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Gender</th>
+                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Phone</th>
+                                {/* <th className="py-4 border-b-2 border-gray-300 text-left text-sm leading-4 tracking-wider uppercase">Email</th> */}
+								<th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Complaint</th>
+                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Status</th>
+                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Details</th>
+								<th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">PDF</th>
+							</tr>
                         </thead>
                         <tbody className="bg-white">
 						{patients.length > 0 ? (
@@ -97,25 +109,29 @@ function TableTest() {
                                             </div>
                                         </div>
                                     </td>  */}
-									<td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-left">
-                                        <div className="text-sm leading-5 text-blue-900 font-bold">{patient.Name}</div>
+									<td className="pl-4 py-4 whitespace-no-wrap border-b border-gray-500 text-left font-semibold">
+                                        <div className="text-sm leading-5 font-bold">{patient.Name}</div>
                                     </td>
-									<td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-center">
-                                        <div className="text-sm leading-5 text-blue-900">{patient.Age}</div>
+									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-center font-semibold">
+                                        <div className="text-sm leading-5">{patient.Age}</div>
                                     </td>
-									<td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-center">
-                                        <div className="text-sm leading-5 text-blue-900">{patient.Gender}</div>
+									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-center font-semibold">
+                                        <div className="text-sm leading-5">{patient.Gender}</div>
                                     </td>
-									<td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5 text-center">{patient["Phone Number"]}</td>
-									<td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5 text-left">{patient.email}</td>
-									<td className="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5 text-center">
-                                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight text-center">
-                                        <span aria-hidden className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                        <span className="relative text-xs">visited</span>
+									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-center font-semibold">{patient["Phone Number"]}</td>
+									{/* <td className="py-4 pr-0 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-left font-semibold">{patient.email}</td> */}
+									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-center font-semibold">{patient.complaints}</td>
+									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-center">
+                                        <span className="relative inline-block px-3 py-1 font-semibold leading-1 text-center">
+                                        <span aria-hidden className="absolute inset-0 bg-red-500 rounded-full"></span>
+                                        <span className="relative text-xs text-white tacking-widest">Completed</span>
                                     </span>
 									</td>
-									<td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5 text-center">
-                                        <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">View Details</button>
+									<td className="py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5 text-center">
+                                        <button className="px-2 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">View Details</button>
+                                    </td>
+									<td className="py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5 text-center">
+                                        <button className="px-2 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Download</button>
                                     </td>
 																		
 								</tr>
@@ -133,7 +149,7 @@ function TableTest() {
 
 				<div className="p-10 text-center">
 				
-				<button className="bg-gray-200 border-none p-2 mx-3 rounded-lg">
+				<button className="bg-blue-500 border-none p-2 mx-3 rounded-lg">
 				<FontAwesomeIcon
 					icon={faChevronLeft}
 					className='text-white'
@@ -141,9 +157,9 @@ function TableTest() {
 				></FontAwesomeIcon>
 				</button>
 
-				<input type="date" name="date" className="border-none p-2 rounded-lg bg-gray-200 text-white" value={{today}}></input>
+				<input type="date" name="date" className="border-none p-2 rounded-lg bg-blue-500 text-white"></input>
 				
-				<button className="border-none bg-gray-200 p-2 mx-3 rounded-lg">
+				<button className="border-none bg-blue-500 p-2 mx-3 rounded-lg">
 				<FontAwesomeIcon
 					icon={faChevronRight}
 					className='text-white'
@@ -160,7 +176,7 @@ function TableTest() {
 					icon={faUserPlus}
 					className='text-blue-700 userplus'
 					size='2x'
-			></FontAwesomeIcon>
+		></FontAwesomeIcon>
 
 			</div>
 
