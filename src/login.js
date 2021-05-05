b34aadcaa1b6532dd3335828a88d3eab127d697b
karthikeyan2002 <bkarthi1000@gmail.com>
2021-05-05 18:20:31 +0530

    ui

--- src/login.js
+++ src/login.js
@@ -7,10 +7,7 @@ function login() {
     <div>
       <div className="login">
         <div className="login-container">
-          <img
-            src={logo}
-          ></img>
-          <p>Admin Login</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,224L60,240C120,256,240,288,360,298.7C480,309,600,299,720,277.3C840,256,960,224,1080,181.3C1200,139,1320,85,1380,58.7L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
           <label>
             <input type="text" name="username" placeholder="Enter User Name" />
           </label>
