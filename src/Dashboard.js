f7b8a9ff2fffc08c3b6b20a58f0ba4f238773c2a
karthikeyan2002 <bkarthi1000@gmail.com>
2021-05-05 11:19:25 +0530

    some changes

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { getPatients } from "./utils/index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-<<<<<<< HEAD:src/TableTest.js
 import {
 	faPowerOff,
 	faBars,
@@ -12,13 +11,7 @@ import {
 	faUserPlus,
 	faChevronCircleUp,
 } from "@fortawesome/free-solid-svg-icons";
-import "./Styles/TableTest.css";
-import { divide } from "lodash";
-import { Button } from "react-bootstrap";
-=======
-import { faPowerOff, faSearch , faChevronLeft , faChevronRight , faUserPlus , faChevronCircleUp , faPen} from "@fortawesome/free-solid-svg-icons";
-import "./Styles/Dashboard.css"
->>>>>>> 6c515854bf9a69c915e4e825087abbbdd178d47d:src/Dashboard.js
+import "./Styles/Dashboard.css";
 
 function Dashboard() {
 	const [patients, setPatients] = useState([]);
@@ -43,79 +36,15 @@ function Dashboard() {
 						size='lg'
 					></FontAwesomeIcon>
 
-<<<<<<< HEAD:src/TableTest.js
 					<div className='text'>
 						<h1>RANGAA DENTAL</h1>
 						<h2> PATIENT DETAILS </h2>
 					</div>
 
 					<p className='date'>{new Date().toLocaleString() + ""}</p>
-=======
-		<div className="Dashboard">
-			
-			<div className="heading">
-			
-				<div className="dropdown">
-					<a>
-						<FontAwesomeIcon
-							icon={faPowerOff}
-							className='position-absolute right-0 text-white'
-							size='lg'
-						></FontAwesomeIcon>
-					</a	>
-					<div class="dropdown-content">
-						<a href="#">Log out</a>
-					</div>			
-				</div>
-
-								
-			
-				<div className="text">
-					<h1>RANGAA DENTAL</h1>
-					<h2> PATIENT DETAILS </h2>
-				</div>
-				
-				<p className="date">
-					{new Date().toLocaleString() + ""}
-				</p>
-
-				<p className="patient"> Number of patients visited : 200 </p>
-
-				<a href="#section2">
-				<FontAwesomeIcon
-					icon={faChevronCircleUp}
-					className='text-white absolute -bottom-3 right-4'
-					size='2x'
-				></FontAwesomeIcon>
-				</a>
-				
-		</div>
-
-		
-<div className="section2" id="section2">
-
-	<div className="w-1/6 float-right search-icon">
-	<div className="p-8">
-	<div className="bg-white flex items-center rounded-full shadow-xl w-full h-12">
-		<input className="rounded-l-full w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none border-none" id="search" type="text" placeholder="Search">
-		</input>
-		<div className="p-4">
-		<button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-6 h-6 flex items-center justify-center">
-		<FontAwesomeIcon
-						icon={faSearch}
-						className='float-right text-white'
-		></FontAwesomeIcon>
-		</button>
-		</div>
-		</div>
-	</div>
-	</div>
-	
->>>>>>> 6c515854bf9a69c915e4e825087abbbdd178d47d:src/Dashboard.js
 
 					<p className='patient'> Number of patients visited : 200 </p>
 
-<<<<<<< HEAD:src/TableTest.js
 					<a href='#section2'>
 						<FontAwesomeIcon
 							icon={faChevronCircleUp}
@@ -264,96 +193,6 @@ function Dashboard() {
 							</button>
 						</div>
 					</div>
-=======
-	
-<div className="-my-2 py-2 overflow-x-hidden sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8 patienttabel clear-both">
-		
-                <div className="align-middle inline-block min-w-full shadow overflow-hidden shadow-dashboard rounded-bl-lg rounded-br-lg">
-                    <table className="min-w-full">
-                        <thead className="text-white">
-                            <tr>
-                                <th className="pl-4 py-4 border-b-2 border-gray-300 text-left leading-4  tracking-wider uppercase">Full Name</th>
-                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Age</th>
-								<th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Gender</th>
-                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Phone</th>
-                                {/* <th className="py-4 border-b-2 border-gray-300 text-left text-sm leading-4 tracking-wider uppercase">Email</th> */}
-								<th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Complaint</th>
-                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Status</th>
-                                <th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Details</th>
-								<th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">PDF</th>
-								<th className="py-4 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider uppercase">Edit</th>
-							</tr>
-                        </thead>
-                        <tbody className="bg-white">
-						{patients.length > 0 ? (
-							patients.map((patient) => (
-								<tr>
-									<td className="pl-4 py-4 whitespace-no-wrap border-b border-gray-500 text-left font-semibold">
-                                        <div className="text-sm leading-5 font-bold">{patient.Name}</div>
-                                    </td>
-									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-center font-semibold">
-                                        <div className="text-sm leading-5">{patient.Age}</div>
-                                    </td>
-									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-center font-semibold">
-                                        <div className="text-sm leading-5">{patient.Gender}</div>
-                                    </td>
-									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-center font-semibold">{patient["Phone Number"]}</td>
-									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-center font-semibold">{patient.complaints}</td>
-									<td className="py-4 whitespace-no-wrap border-b border-gray-500 text-sm leading-5 text-center">
-                                        <span className="relative inline-block px-3 py-1 font-semibold leading-1 text-center">
-                                        <span aria-hidden className="absolute inset-0 bg-red-500 rounded-full"></span>
-                                        <span className="relative text-xs text-white tacking-widest">Completed</span>
-                                    </span>
-									</td>
-									<td className="py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5 text-center">
-                                        <button className="px-2 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">View Details</button>
-                                    </td>
-									<td className="py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5 text-center">
-                                        <button className="px-2 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Download</button>
-                                    </td>
-									<td className="py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5 text-center">
-									<a>
-									<FontAwesomeIcon
-										icon={faPen}
-									></FontAwesomeIcon>
-									</a>
-									</td>
-								
-																
-								</tr>
-							))
-						) : (
-							<h2 style={{ fontFamily: "monospace" }}>
-								Loading ....
-							</h2>
-						)}
-                                
-                       </tbody>
-                    </table>
-            
-                </div>
-
-				<div className="p-10 text-center">
-				
-				<button className="bg-blue-500 border-none p-2 mx-3 rounded-lg">
-				<FontAwesomeIcon
-					icon={faChevronLeft}
-					className='text-white'
-					size='lg'
-				></FontAwesomeIcon>
-				</button>
-
-				<input type="date" name="date" className="border-none p-2  text-center rounded-lg bg-blue-500 text-white"></input>
-				
-				<button className="border-none bg-blue-500 p-2 mx-3 rounded-lg">
-				<FontAwesomeIcon
-					icon={faChevronRight}
-					className='text-white'
-					size='lg'
-				></FontAwesomeIcon>
-				</button>
-
->>>>>>> 6c515854bf9a69c915e4e825087abbbdd178d47d:src/Dashboard.js
 				</div>
 
 				<FontAwesomeIcon
